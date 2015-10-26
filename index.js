745cf4a533c76cc0c69ec9784f23548e2c681de1
Doron Segal <doron2402@gmail.com>
2015-10-26 12:41:17 -0700

    starting

---
+++ index.js
@@ -0,0 +1,108 @@
+module.exports = function(options) {
+
+  var Promise = require("bluebird");
+  var superagent = require("superagent");
+  var _ = require('lodash');
+  var Request = superagent.Request;
+  var ACTIONS = ['GET', 'DELETE', 'HEAD', 'PATCH', 'POST', 'PUT'];
+  var HEADERS = (options.headers && options.headers.base) ? options.headers.base : {};
+  var GET_HEADERS = (options.headers && options.headers.get) ? options.headers.get : {};
+  var POST_HEADERS = (options.headers && options.headers.post) ? options.headers.post : {};
+  var DELETE_HEADERS = (options.headers && options.headers.del) ? options.headers.del : {};
+  var PUT_HEADERS = (options.headers && options.headers.put) ? options.headers.put : {};
+
+  // Create custom error type.
+  // Create a new object, that prototypally inherits from the Error constructor.
+  var SuperagentPromiseError = superagent.SuperagentPromiseError = function (message) {
+    this.name = 'SuperagentPromiseError';
+    this.message = message || 'Bad request';
+  };
+
+  SuperagentPromiseError.prototype = new Error();
+  SuperagentPromiseError.prototype.constructor = SuperagentPromiseError;
+
+  /**
+   * @namespace utils
+   * @class Superagent
+   *
+   * Add promise support for superagent/supertest
+   *
+   * Call .promise() to return promise for the request
+   *
+   * @method then
+   * @return {Bluebird.Promise}
+   */
+  Request.prototype.promise = function() {
+    var req = this;
+    var error;
+
+    return new Promise(function(resolve, reject) {
+        req.end(function(err, res) {
+          if (typeof res !== "undefined" && res.status >= 400) {
+            var msg = 'cannot ' + req.method + ' ' + req.url + ' (' + res.status + ')';
+            error = new SuperagentPromiseError(msg);
+            error.status = res.status;
+            error.body = res.body;
+            error.res = res;
+            reject(error);
+          } else if (err) {
+            reject(new SuperagentPromiseError(err));
+          } else {
+            resolve(res);
+          }
+        });
+      })
+      .cancellable()
+      .caught(Promise.CancellationError, function(err) {
+        req.abort();
+        throw err;
+      });
+  };
+
+  /**
+   *
+   * Make superagent requests Promises/A+ conformant
+   *
+   * Call .then([onFulfilled], [onRejected]) to register callbacks
+   *
+   * @method then
+   * @param {function} [onFulfilled]
+   * @param {function} [onRejected]
+   * @return {Bluebird.Promise}
+   */
+  Request.prototype.then = function() {
+    var promise = this.promise();
+    return promise.then.apply(promise, arguments);
+  };
+
+  function httpCall(action) {
+    return function(url, data){
+      var headers = {};
+      if (action.toLowerCase() === 'get') {
+        //GET
+        headers = _.assign(HEADERS, GET_HEADERS);
+      } else if (action.toLowerCase() === 'post') {
+        //POST
+        headers = _.assign(HEADERS, POST_HEADERS);
+      } else if  ('del'.indexOf(action.toLowerCase()) !== -1) {
+        //DELETE
+        headers = _.assign(HEADERS, DELETE_HEADERS);
+      } else {
+        //PUT
+        headers = _.assign(HEADERS, PUT_HEADERS);
+      }
+      var currentRequest = superagent(action, url);
+      currentRequest.set(headers);
+      currentRequest.query(data);
+      return _.assign(currentRequest,{ method: action.toLowerCase(), url: url });
+    };
+  }
+
+  ACTIONS.forEach(function(action){
+    var name = (action === 'DELETE') ? 'del' : action.toLowerCase();
+    superagent[name] = httpCall(action);
+  });
+
+  return superagent;
+};
+
---
+++ test/index.js
@@ -0,0 +1,96 @@
+var chai = require('chai');
+var expect = chai.expect;
+var sinon = require('sinon');
+var Promise = require('bluebird');
+var _ = require('lodash');
+var Interfake = require('interfake');
+var interfake = new Interfake();
+var BASE_HEADER = {'service-type': 'Doron-Service'};
+var GET_HEADER = { 'type': 'get-test-header' };
+var POST_HEADER = { 'type': 'post-test-header' };
+var request = require('../')({
+	headers: {
+		base: BASE_HEADER,
+		get: GET_HEADER,
+		post: POST_HEADER
+	}
+});
+var PORT = 9987;
+interfake.get('/good').body({code: 'ok'});
+interfake.post('/good').body({code: 'ok'});
+interfake.get('/bad').status(400).body({code: 'error', error: 'error'});
+interfake.get('/not-found').status(404).body({error: 'not found'});
+interfake.listen(PORT);
+
+describe('OT-SuperAgent-Promise', function(){
+	describe('Request Object', function(){
+		it('Request Should exist', function(){
+			expect(request).to.exist;
+		});
+	});
+
+	describe('When [GET] /good', function(){
+		it('Should return a promise', function(done){
+			request.get('localhost:' + 9987 + '/good')
+			.then(function(res){
+				expect(res.req._header).to.contains(BASE_HEADER['service-type']);
+				expect(res.req._header).to.contains(GET_HEADER['type']);
+				expect(res).to.exist;
+        expect(res.status).to.equal(200);
+        expect(res.body.code).to.equal('ok');
+        done();
+			}).catch(function(e){
+				done();
+			});
+		});
+	});
+
+	describe('When [POST] /good', function(){
+		it('Should return a promise', function(done){
+			request.post('localhost:' + 9987 + '/good')
+			.then(function(res){
+				expect(res.req._header).to.contains(BASE_HEADER['service-type']);
+				expect(res.req._header).to.contains(POST_HEADER['type']);
+				expect(res).to.exist;
+        expect(res.status).to.equal(200);
+        expect(res.body.code).to.equal('ok');
+        done();
+			}).catch(function(e){
+				done();
+			});
+		});
+	});
+
+	describe('When [GET] /bad', function(){
+		it('Should return a promise', function(done){
+			request.get('localhost:' + 9987 + '/bad')
+			.then(function(res){
+				expect(res).to.not.exist;
+				done();
+			}).catch(function(e){
+				expect(e).to.exist;
+				expect(e.res.req._header).to.contains(BASE_HEADER['service-type']);
+				expect(e.res.req._header).to.contains(GET_HEADER['type']);
+				done();
+			});
+		});
+	});
+
+	describe('When [GET] /not-found', function(){
+		it('Should reject an error', function(done){
+			request.get('localhost:' + 9987 + '/not-found')
+			.then(function(res){
+				expect(res).to.not.exist;
+				done();
+			}).catch(function(error){
+				expect(error).to.exist;
+        expect(error.res).to.exist;
+        expect(error.status).to.exist;
+        expect(error.body).to.exist;
+        expect(error).to.be.instanceof(Error);
+        expect(error.name).to.equal("SuperagentPromiseError");
+        done();
+			});
+		});
+	});
+});
